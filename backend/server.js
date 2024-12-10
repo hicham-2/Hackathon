54c3af9ec8e0327de3bb2bce1c0fb4b0a0ff9150
Hicham AISSAOUI <aissaoui.hicham00@gmail.com>
2024-12-10 16:01:04 +0100

    setup project

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Charger les variables d'environnement
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
