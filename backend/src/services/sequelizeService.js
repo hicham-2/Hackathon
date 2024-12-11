c5775f6c8dd8f007e1e1644683b82d25c4cbb3e5
Louafi Razine <louafi.razine@gmail.com>
2024-12-11 17:12:26 +0100

    add notification service/model and add handleassocitions

--- backend/src/services/sequelizeService.js
+++ backend/src/services/sequelizeService.js
@@ -1,23 +1,41 @@
 import { Sequelize } from "sequelize";
 import { UserService } from "./userService.js";
+import { NotificationService } from "./notificationService.js";
 
 export class SequelizeService {
   constructor(sequelize) {
     this.sequelize = sequelize;
     this.userService = new UserService(this);
+    this.notificationService = new NotificationService(this);
+  }
+
+  defineAssociations() {
+    this.notificationService.model.belongsTo(this.userService.model, {
+      foreignKey: "user_id",
+      as: "user",
+      onDelete: "CASCADE",
+    });
+
+    this.userService.model.hasMany(this.notificationService.model, {
+      foreignKey: "user_id",
+      as: "notifications",
+    });
   }
 
   async synchronize() {
     await this.sequelize.sync({ force: true });
   }
-  
+
   static async get() {
     if (this.instance !== undefined) {
       return this.instance;
     }
     const connection = await this.openConnection();
     this.instance = new SequelizeService(connection);
+
+    this.instance.defineAssociations();
     this.instance.synchronize();
+
     return this.instance;
   }
 
@@ -29,5 +47,4 @@ export class SequelizeService {
       throw error;
     }
   }
-
 }
